39a9cf99dbc1cbaf25d7992b2423eba020bca723
Oscardev <oscardev@gmail.com>
2024-09-25 09:49:46 -0400

    Fix: Swiper testimonial overflow in mobile bug fixed

--- src/components/SwipperUsers.jsx
+++ src/components/SwipperUsers.jsx
@@ -12,10 +12,10 @@ export default function SwipperUsers() {
       <Swiper pagination={true}  modules={[Pagination]}>
         {users.map((user) => (
           <SwiperSlide>
-            <div class="card relative max-w-[300px] bg-veryLightGray rounded-md p-5 mb-12 mx-auto">
+            <div class="card relative max-w-[300px] bg-veryLightGray rounded-md py-3 mb-12 mx-auto">
               <div class="card-header flex items-center justify-center">
                 <img
-                  class="w-16 h-w-16 rounded-full block absolute -top-10"
+                  class="w-16 h-w-16 rounded-full block"
                   src={user.imageURL}
                   alt={user.usename}
                 />
